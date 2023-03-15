ba6a68749709974b806c9909052828a2b6e1c54b
markov <mkvph0ch@pm.me>
2023-03-15 20:53:57 +0100

    clean LH data

---
+++ src/create_tables.sql
@@ -0,0 +1,39 @@
+CREATE TABLE countries (
+    CountryCode char(2) NOT NULL,
+    CountryName text NOT NULL,
+    PRIMARY KEY (CountryCode)
+); 
+CREATE TABLE cities (
+    CityCode char(3) NOT NULL,
+    CountryCode char(2) NOT NULL,
+    UtcOffset time NOT NULL,
+    TimeZoneId time NOT NULL,
+    PRIMARY KEY (CityCode),
+    FOREIGN KEY (CountryCode) REFERENCES countries (CountryCode)
+); 
+CREATE TABLE airports (
+    AirportCode char(3) NOT NULL,
+    CityCode char(3) NOT NULL,
+    CountryCode char(2) NOT NULL,
+    LocationType text,
+    UtcOffset time NOT NULL,
+    TimeZoneId time NOT NULL,
+    Latitude double precision NOT NULL,
+    Longitude double precision NOT NULL,
+    AirportName text NOT NULL,
+    PRIMARY KEY (AirportCode),
+    FOREIGN KEY (CityCode) REFERENCES cities (CounCityCodetryCode),
+    FOREIGN KEY (CountryCode) REFERENCES countries (CountryCode)
+); 
+CREATE TABLE airlines (
+    AirlineID char(2) NOT NULL,
+    AirlineID_ICAO char(3),
+    AirlineName text NOT NULL,
+    PRIMARY KEY (AirlineID)
+); 
+CREATE TABLE aircrafts (
+    AircraftCode char(3) NOT NULL,
+    AirlineEquipCode char(4) NOT NULL,
+    AircraftName text NOT NULL,
+    PRIMARY KEY (AircraftCode)
+); 
