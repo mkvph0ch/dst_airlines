3f8c0360c6079243b40fc5b80754b4f6df355983
markov <mkvph0ch@pm.me>
2023-03-17 01:07:04 +0100

    updated sql create tables

--- src/create_tables.sql
+++ src/create_tables.sql
@@ -1,27 +1,27 @@
 CREATE TABLE countries (
     CountryCode char(2) NOT NULL,
-    CountryName text NOT NULL,
+    CountryName text,
     PRIMARY KEY (CountryCode)
 ); 
 CREATE TABLE cities (
     CityCode char(3) NOT NULL,
-    CountryCode char(2) NOT NULL,
-    UtcOffset interval NOT NULL,
-    TimeZoneId text NOT NULL,
-    CityName text NOT NULL,
+    CountryCode char(2),
+    UtcOffset interval,
+    TimeZoneId text,
+    CityName text,
     PRIMARY KEY (CityCode),
     FOREIGN KEY (CountryCode) REFERENCES countries (CountryCode)
 ); 
 CREATE TABLE airports (
     AirportCode char(3) NOT NULL,
-    CityCode char(3) NOT NULL,
-    CountryCode char(2) NOT NULL,
+    CityCode char(3),
+    CountryCode char(2),
     LocationType text,
-    UtcOffset interval NOT NULL,
-    TimeZoneId text NOT NULL,
-    Latitude double precision NOT NULL,
-    Longitude double precision NOT NULL,
-    AirportName text NOT NULL,
+    UtcOffset interval,
+    TimeZoneId text,
+    Latitude double precision,
+    Longitude double precision,
+    AirportName text,
     PRIMARY KEY (AirportCode),
     FOREIGN KEY (CityCode) REFERENCES cities (CounCityCodetryCode),
     FOREIGN KEY (CountryCode) REFERENCES countries (CountryCode)
@@ -29,12 +29,12 @@ CREATE TABLE airports (
 CREATE TABLE airlines (
     AirlineID char(2) NOT NULL,
     AirlineID_ICAO char(3),
-    AirlineName text NOT NULL,
+    AirlineName text,
     PRIMARY KEY (AirlineID)
 ); 
 CREATE TABLE aircrafts (
     AircraftCode char(3) NOT NULL,
-    AirlineEquipCode char(4) NOT NULL,
-    AircraftName text NOT NULL,
+    AirlineEquipCode char(4),
+    AircraftName text,
     PRIMARY KEY (AircraftCode)
 ); 
