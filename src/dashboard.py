e5a0383b53729506a8ad64e3541a4df8f16d201e
markov <mkvph0ch@pm.me>
2023-04-04 18:23:23 +0200

    changes figsize and margins

--- src/dashboard.py
+++ src/dashboard.py
@@ -5,8 +5,9 @@ import plotly.graph_objects as go
 import pandas as pd
 import globals
 import requests
-from mongodb import get_geopositions_from_airlabs
+from mongodb import get_geopositions_from_airlabs, load_positions_from_airlabs
 import time
+import datetime
 from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
@@ -19,6 +20,7 @@ app.layout = html.Div(children=[
     html.Div(children='''
         This data was provided by the Airlabs API https://airlabs.co.
     '''),
+    html.Div(children="This project was created by Moldir, Erntam, Sam, Marko"),
 
     dcc.Graph(
         id='world-map'
@@ -33,9 +35,10 @@ app.layout = html.Div(children=[
 
 # Multiple components can update everytime interval gets fired.
 @app.callback(Output('world-map', 'figure'),
-              Input('interval-component', 'n_intervals'))
+              [Input('interval-component', 'n_intervals')])
 def update_graph_live(n):
     _, df = get_geopositions_from_airlabs(globals.airlabs_token)
+    last_timestamp=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     # Create the graph with subplots
     fig = go.Figure(data=go.Scattergeo(
@@ -47,7 +50,17 @@ def update_graph_live(n):
 
     fig.update_layout(
         geo_scope='world',
-        title_text=f"Interval {n}"
+        autosize=False,
+        width=1400,
+        height=800,
+        margin=dict(
+        l=200,
+        r=10,
+        b=10,
+        t=100,
+        pad=4
+        ),
+        title_text=f"Interval {n}. Last Update {last_timestamp}"
     )
 
     return fig
