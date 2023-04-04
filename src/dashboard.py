4100a2042d9e62f58eb7d198fc3bd9623070d9f4
markov <mkvph0ch@pm.me>
2023-04-04 21:25:05 +0200

    update dashboard.py

--- src/dashboard.py
+++ src/dashboard.py
@@ -9,46 +9,63 @@ from mongodb import get_geopositions_from_airlabs, load_positions_from_airlabs
 import time
 import datetime
 from dash.dependencies import Input, Output
-
+​
 app = dash.Dash(__name__)
 globals.initialize()
-
+​
 #_, df = get_geopositions_from_airlabs(globals.airlabs_token)
-
+​
 app.layout = html.Div(children=[
     html.H1(children='DST Airlines'),
     html.Div(children='''
         This data was provided by the Airlabs API https://airlabs.co.
     '''),
     html.Div(children="This project was created by Moldir, Erntam, Sam, Marko"),
-
+​
     dcc.Graph(
         id='world-map'
     ),
-
+​
     dcc.Interval(
         id='interval-component',
         interval=35*1000, # in milliseconds
         n_intervals=0
     )
 ])
-
+​
 # Multiple components can update everytime interval gets fired.
 @app.callback(Output('world-map', 'figure'),
               [Input('interval-component', 'n_intervals')])
 def update_graph_live(n):
     _, df = get_geopositions_from_airlabs(globals.airlabs_token)
     last_timestamp=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
+​
     # Create the graph with subplots
     fig = go.Figure(data=go.Scattergeo(
         lon=df['lng'],
         lat=df['lat'],
+        # text=df['airline_iata'] + df['flight_number'],
         text=df['flight_number'],
-        mode='markers'
-    ))
-
+        mode='markers',
+        # marker=dict(
+        #     color='blue'
+        # ),
+        geojson='https://raw.githubusercontent.com/python-visualization/folium/master/examples/data'
+                '/world-countries.json',
+        featureidkey='properties.name',
+        texttemplate='%{properties.name}', # use the country name from the GeoJSON file as text
+        showlegend=True,
+     ))
+​
     fig.update_layout(
+        geo=dict(
+            # landcolor="LightGreen",
+            # oceancolor="LightBlue",
+            # showocean=True,
+            # showland=True,
+            showcountries=True,
+            countrycolor="Black"
+        ),
         geo_scope='world',
         autosize=False,
         width=1400,
@@ -62,9 +79,8 @@ def update_graph_live(n):
         ),
         title_text=f"Interval {n}. Last Update {last_timestamp}"
     )
-
+​
     return fig
-
+​
 if __name__ == '__main__':    
     app.run_server(debug=True)
-    
