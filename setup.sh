09092628a626546dfd280f40302d9d09878dd9a7
mkvph0ch <62255328+mkvph0ch@users.noreply.github.com>
2023-04-25 14:24:34 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-sudo docker build . -t dst_airlines_api:latest -f Dockerfile.api
-sudo docker build . -t dst_airlines_dash:latest -f Dockerfile.dash
+#sudo docker build . -t dst_airlines_api:latest -f Dockerfile.api # Optional
+#sudo docker build . -t dst_airlines_dash:latest -f Dockerfile.dash # Optional
 sudo docker-compose up --build -d
+
+# Determine the name of the python binary
+if command -v python3 &>/dev/null; then
+    PY=python3 # for e.g. Ubuntu and Debian-based Distros
+elif command -v python &>/dev/null; then
+    PY=python # for e.g. fedora linux
+else
+    echo "Python not found"
+    exit 1
+fi
+
+# Run the python files
+$PY src/pandas_to_psql.py
+$PY src/mongodb.py
